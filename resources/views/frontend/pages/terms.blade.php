32cb2de80df427fe6c9cd246d71392e62dcc0f10
Hasan Aftab <hasanaftab007@gmail.com>
2024-03-15 02:50:29 +0500

    Integrate new layout for terms and conditions page

--- resources/views/frontend/pages/terms.blade.php
+++ resources/views/frontend/pages/terms.blade.php
@@ -1,19 +1,13 @@
-@extends('frontend.layout.homepagelayout')
+@extends('frontend.layout.homepagenew')
 
 @section('content')
-<!-- ========== End header ========== -->
+    <!-- ========== End header ========== -->
 
-    <div class="conflicts-heading">
-        <div class="container">
-            <div class="text">
-                <h4> Track </h4>
-            </div>
-        </div>
-    </div>
-    
+    <h1 style="text-align: center"> Terms & Conditions </h1>
+
     <div class="container" style="padding-top: 38px;font-size: 21px;">
-            <div class="text">
-                {!! $terms->description !!}
-            </div>
+        <div class="text">
+            {!! $terms->description !!}
+        </div>
     </div>
 @endsection
