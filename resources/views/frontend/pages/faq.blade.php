aa7ad95d29b1b4d57d425c4988a912a73fd75b23
Hasan Aftab <hasanaftab007@gmail.com>
2024-03-14 19:58:05 +0500

    FAQ design page

--- resources/views/frontend/pages/faq.blade.php
+++ resources/views/frontend/pages/faq.blade.php
@@ -2,94 +2,46 @@
 
 @section('content')
 
-<style type="text/css">
-@import url("https://fonts.googleapis.com/css2?family=Sora:wght@100;200;300;400;500;600;700&display=swap");
-body {
-    background: #fafafa;
-}
-.accordion {
-    display: flex;
-    flex-direction: column;
-    font-family: "Sora", sans-serif;
-    max-width: 991px;
-    min-width: 320px;
-    margin: 50px auto;
-    padding: 0 50px;
-}
-.accordion h1 {
-    font-size: 32px;
-    text-align: center;
-}
-.accordion-item {
-    margin-top: 16px;
-    border: 1px solid #fcfcfc;
-    border-radius: 6px;
-    background: #ffffff;
-    box-shadow: rgba(0, 0, 0, 0.05) 0px 1px 2px 0px;
-}
-.accordion-item .accordion-item-title {
-    position: relative;
-    margin: 0;
-    width: 100%;
-    font-size: 15px;
-    cursor: pointer;
-    justify-content: space-between;
-    flex-direction: row-reverse;
-    padding: 14px 20px;
-    box-sizing: border-box;
-    align-items: center;
-}
-.accordion-item .accordion-item-desc {
-    display: none;
-    font-size: 14px;
-    line-height: 22px;
-    font-weight: 300;
-    color: #444;
-    border-top: 1px dashed #ddd;
-    padding: 10px 20px 20px;
-    box-sizing: border-box;
-}
-.accordion-item input[type="checkbox"] {
-    position: absolute;
-    height: 0;
-    width: 0;
-    opacity: 0;
-}
-.accordion-item input[type="checkbox"]:checked ~ .accordion-item-desc {
-    display: block;
-}
-.accordion-item
-    input[type="checkbox"]:checked
-    ~ .accordion-item-title
-    .icon:after {
-    content: "-";
-    font-size: 20px;
-}
-.accordion-item input[type="checkbox"] ~ .accordion-item-title .icon:after {
-    content: "+";
-    font-size: 20px;
-}
-.accordion-item:first-child {
-    margin-top: 0;
-}
-.accordion-item .icon {
-    margin-left: 14px;
-}
-
-@media screen and (max-width: 767px) {
-    .accordion {
-        padding: 0 16px;
-    }
-    .accordion h1 {
-        font-size: 22px;
-    }
-}
-</style>
-    <section class="Stand">
-        <div class="container">
-            <h4>FAQ</h4>
+    <section class="main-section full-container">
+        <div class="container flex l-gap  flex-mobile">
+            <div class="cta-sidebar">
+                <div><p>Stay on top of <span style="color: #8529cd; width:auto;">Rakuten</span> deals effortlessly with
+                        our tracking and alert system. Never miss out on savings again.</p><a
+                        href="https://www.rakuten.com/r/CARRIE21277?eeid=28187&amp;fbclid=IwAR1nvZOOBFIuGjHq-IaiM73dK8iVQaHBqHWOpa--7xWwPwdWYhSXTdoTMVw"
+                        class="cta-btn">Join Now!</a></div>
+                <div><p>Already saving with TrackRak?</p><a
+                        href="https://www.rakuten.com/r/CARRIE21277?eeid=28187&amp;fbclid=IwAR1nvZOOBFIuGjHq-IaiM73dK8iVQaHBqHWOpa--7xWwPwdWYhSXTdoTMVw"
+                        class="cta-btn">Login Now!</a></div>
+            </div>
+            <div class="page-content pg-l"><h1 class="page-title">FAQ<span class="sm-ls">s</span></h1>
+                <div class="accordion-app">
+                    <button class="accordion">
+                        HEADING NO QA1
+                    </button>
+                    <div class="panel">
+                        {!! $faq->description !!}
+                    </div>
+                    <button class="accordion">
+                        HEADING NO QA2
+                    </button>
+                    <div class="panel">
+                        {!! $faq->description !!}
+                    </div>
+                    <button class="accordion">
+                        HEADING NO QA3
+                    </button>
+                    <div class="panel">
+                        {!! $faq->description !!}
+                    </div>
+                    <button class="accordion">
+                        HEADING NO QA4
+                    </button>
+                    <div class="panel">
+                        {!! $faq->description !!}
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
-
-    {!! $faq->description !!}
+    {{--    {!! $faq->description !!}--}}
 @endsection
