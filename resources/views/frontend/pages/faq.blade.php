818e365f3dbbab6951d3f5f019d213760d16fa53
Hasan Aftab <hasanaftab007@gmail.com>
2024-03-16 02:41:15 +0500

    Update faq page

--- resources/views/frontend/pages/faq.blade.php
+++ resources/views/frontend/pages/faq.blade.php
@@ -3,45 +3,37 @@
 @section('content')
 
     <section class="main-section full-container">
-        <div class="container flex l-gap  flex-mobile">
+        <div class="container flex l-gap flex-mobile lr-m">
             <div class="cta-sidebar">
-                <div><p>Stay on top of <span style="color: #8529cd; width:auto;">Rakuten</span> deals effortlessly with
-                        our tracking and alert system. Never miss out on savings again.</p><a
-                        href="https://www.rakuten.com/r/CARRIE21277?eeid=28187&amp;fbclid=IwAR1nvZOOBFIuGjHq-IaiM73dK8iVQaHBqHWOpa--7xWwPwdWYhSXTdoTMVw"
-                        class="cta-btn">Join Now!</a></div>
-                <div><p>Already saving with TrackRak?</p><a
-                        href="https://www.rakuten.com/r/CARRIE21277?eeid=28187&amp;fbclid=IwAR1nvZOOBFIuGjHq-IaiM73dK8iVQaHBqHWOpa--7xWwPwdWYhSXTdoTMVw"
-                        class="cta-btn">Login Now!</a></div>
+                <div>
+                    <p><span class="tagline">TrackRak &amp;<br>Get More<br>Money Back!</span><br>
+                        <br>
+                        Stay on top of <a style="color: #8529cd; width:auto; font-weight: 600; text-decoration: none;" href="http://tinyurl.com/d98frkfy" target="_blank">Rakuten</a> deals with our alert tool. Never miss out on savings again!<br>
+                        <br>
+                        Your first alert is <strong>FREE!</strong></p>
+                    <a href="#" class="cta-btn">Join now!</a>
+                </div>
+                <div>
+                    <p>Already saving with TrackRak?</p>
+                    <a href="#" class="cta-btn">Login now!</a>
+                </div>
             </div>
-            <div class="page-content pg-l"><h1 class="page-title">FAQ<span class="sm-ls">s</span></h1>
+            <div class="page-content pg-l">
+                <h1 class="page-title">FAQ <span class="sm-ls">s</span>
+                </h1>
                 <div class="accordion-app">
-                    <button class="accordion">
-                        HEADING NO QA1
-                    </button>
-                    <div class="panel">
-                        {!! $faq->description !!}
-                    </div>
-                    <button class="accordion">
-                        HEADING NO QA2
-                    </button>
-                    <div class="panel">
-                        {!! $faq->description !!}
-                    </div>
-                    <button class="accordion">
-                        HEADING NO QA3
-                    </button>
-                    <div class="panel">
-                        {!! $faq->description !!}
-                    </div>
-                    <button class="accordion">
-                        HEADING NO QA4
-                    </button>
-                    <div class="panel">
-                        {!! $faq->description !!}
-                    </div>
+                    <button class="accordion">HEADING NO QA1</button>
+                    <div class="panel">{!! $faq->description !!}</div>
+                    <button class="accordion">HEADING NO QA2</button>
+                    <div class="panel">{!! $faq->description !!}</div>
+                    <button class="accordion">HEADING NO QA3</button>
+                    <div class="panel">{!! $faq->description !!}</div>
+                    <button class="accordion">HEADING NO QA4</button>
+                    <div class="panel">{!! $faq->description !!}</div>
                 </div>
             </div>
         </div>
     </section>
+
     {{--    {!! $faq->description !!}--}}
 @endsection
