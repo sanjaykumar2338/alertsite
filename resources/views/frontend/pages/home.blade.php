0170168c66222dd2412d53449d35dc440b36657e
Sanjay Kumar <sk963070@gmail.com>
2024-04-22 12:29:38 +0530

    new changes

--- resources/views/frontend/pages/home.blade.php
+++ resources/views/frontend/pages/home.blade.php
@@ -5,22 +5,33 @@
     <section class="main-section full-container">
         <div class="container flex l-gap flex-mobile lr-m">
             @includeIf('frontend.layout.sidebar')
-            <div class="page-content">
-            @if(session('success'))
-                <div id="success-message" style="background-color: #d4edda; color: #155724; border-color: #c3e6cb; padding: .75rem 1.25rem; margin-bottom: 1rem; border: 1px solid transparent; border-radius: .25rem;">
-                    {{ session('success') }}
-                </div>
-                <script>
-                    setTimeout(function() {
-                        var element = document.getElementById('success-message');
-                        element.parentNode.removeChild(element);
-                    }, 4000); // Remove after 2 seconds (2000 milliseconds)
-                </script>
-            @endif
+            <div class="page-content home">
+                
+                @if(session('success'))
+                    <div id="success-message" style="background-color: #d4edda; color: #155724; border-color: #c3e6cb; padding: .75rem 1.25rem; margin-bottom: 1rem; border: 1px solid transparent; border-radius: .25rem;">
+                        {{ session('success') }}
+                    </div>
+                    <script>
+                        setTimeout(function() {
+                            var element = document.getElementById('success-message');
+                            element.parentNode.removeChild(element);
+                        }, 4000); // Remove after 2 seconds (2000 milliseconds)
+                    </script>
+                @endif
 
-                <div>
+                {!! $page->description !!}
+
+                <div class="btn-container-submit">
+    <input type="submit" onclick="window.location.href='{{ route('register') }}'" id="submit" class="l-submit" value="Join Now">    <input type="submit" onclick="window.location.href='{{ route('login') }}'" id="submit" class="l-submit" value="Login Now">
+</div>           
+           
+<p></p>
+
+                <!--
+                <div>   
                     {!! $page->description !!}
                 </div>
+                -->
             </div>
         </div>
     </section>
