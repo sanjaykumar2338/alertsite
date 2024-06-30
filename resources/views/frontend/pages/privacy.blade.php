cb7cb0313d0b50b120becfe4a2c60f260e62d922
Sanjay Kumar <sk963070@gmail.com>
2024-06-30 15:06:46 +0530

    new changes

--- resources/views/frontend/pages/privacy.blade.php
+++ resources/views/frontend/pages/privacy.blade.php
@@ -5,7 +5,7 @@
 <section class="main-section full-container">
     <div class="container flex l-gap flex-mobile lr-m">
         @includeIf('frontend.layout.sidebar')
-        <div class="page-content pg-l" style="width:600;">
+        <div class="page-content pg-l" style="width:550;">
             <h1 class="page-title">Privacy Policy</h1>
             </h1>
             <div>
