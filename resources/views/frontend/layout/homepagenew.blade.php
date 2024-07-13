3695f90101054692d05f69e8273db9ea87390fb7
Sanjay Kumar <sanjaykumar2338@yahoo.com>
2024-07-13 13:57:08 +0530

    new changes

--- resources/views/frontend/layout/homepagenew.blade.php
+++ resources/views/frontend/layout/homepagenew.blade.php
@@ -246,6 +246,10 @@
             defaultOption.textContent = defaultText;
             defaultOption.disabled = true;
             defaultOption.selected = true;
+
+            if ($('#current_store_id').length) {
+                $('#store').val($('#current_store_id').val()).trigger('change');
+            }
         }
 
         function initializeSelect2() {
@@ -284,10 +288,6 @@
                                 });
 
                                 select.appendChild(fragment);
-
-                                if ($('#current_store_id').length) {
-                                    $('#store').val($('#current_store_id').val()).trigger('change');
-                                }
                             }                          
 
                             // Reinitialize Select2 after data is loaded
